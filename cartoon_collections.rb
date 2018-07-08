9ea481860661bdf67997acbd185ee5e84df5be6c
Christine Xiang <yutingcxiang@gmail.com>
2018-07-08 05:04:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  
+  array.each do |planeteer|
+    planeteer.capitalize
+  end
 end
 
 def long_planeteer_calls# code an argument here
