eac1bc8edc49abbc3f2cfd7bf4fc7e36d7b961b7
Christine Xiang <yutingcxiang@gmail.com>
2018-07-08 05:07:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(array)
   return planeteers
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array[i]
 end
 
 def find_the_cheese# code an argument here
