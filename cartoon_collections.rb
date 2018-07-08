98a7f583467c857ad0c2cb09920fbb46bde2b78f
Christine Xiang <yutingcxiang@gmail.com>
2018-07-08 05:02:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index|
     dwarves << "#{index}. #{dwarf}"
   end
-  return dwarves.join(",")
+  puts dwarves
 end
 
 def summon_captain_planet# code an argument here
